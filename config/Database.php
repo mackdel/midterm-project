bc8e00ff614b0b98020fa9aae36ba30a30961594
gitmackfree <freemanmackenzie06@gmail.com>
2024-03-15 14:15:45 -0400

    Edit database conn

--- config/Database.php
+++ config/Database.php
@@ -36,29 +36,3 @@
             }
         }
     }
-    /* class Database {
-        // DB Params
-        private $host = 'localhost';
-        private $port = '5432';
-        private $db_name = 'quotesdb';
-        private $username =  'postgres';
-        private $password = 'postgres';
-        private $conn;
-
-        // DB Connect
-        public function connect() {
-            $this->conn = null; 
-            $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
-
-            // connect through PDO
-            try {
-                $this->conn = new PDO($dsn, $this->username, $this->password);
-
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch (PDOException $e) {
-                echo 'Connection Error:' . $e->getMessage();
-            }
-
-            return $this->conn;
-        }
-    } */
