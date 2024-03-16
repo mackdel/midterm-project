2f84e05a9fc42e647df914e576e1137626c659ef
gitmackfree <freemanmackenzie06@gmail.com>
2024-03-16 01:52:20 -0400

    fixing comments

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -2,10 +2,11 @@
     // Get Quote Query Results
     $result = $quote->read_single();
     $num = $result->rowCount();
-    
+
     // Check if Quotes
     if ($num > 0){
-        if (isset($quote->id)) {
+        // Single Quote
+        if (isset($quote->id)) { 
             $row = $result->fetch(PDO::FETCH_ASSOC);
             extract($row);
 
@@ -18,10 +19,12 @@
 
             echo json_encode($quote_arr);
         } else {
-        // Quotes Array
-            // If query used author_id or category_id there might be multiple results
+        // If query used author_id or category_id there might be multiple results
+
+        // Quotes Array created
         $quotes_arr = array();
 
+        // Fill Array W/ Quotes
         while($row = $result->fetch(PDO::FETCH_ASSOC)) {
             extract($row);
 
@@ -46,53 +49,3 @@
             array('message' => 'No Quotes Found')
         );
     }
-
-
-    /* <?php  
-    // Get Quote Query Results
-    $result = $quote->read_single();
-    $num = $result->rowCount();
-    $quotes_arr = array();
-
-    // Check if Quotes
-    if ($num > 0){ // Single Quote
-        if ($num == 1 && $quote->id) {
-            $row = $result->fetch(PDO::FETCH_ASSOC);
-            extract($row);
-
-            // Create Quote Array
-            $quotes_arr = array(
-                'id' => $id,
-                'quote' => html_entity_decode($quote),
-                'author' => $author,
-                'category' => $category
-            );
-
-            // Turn to JSON & Output
-            echo json_encode($quotes_arr);
-
-        } else { // Quotes from Author and/or Category
-            // Quotes Array
-            while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                extract($row);
-
-                // One Quote
-                $quotes_item = array(
-                    'id' => $id,
-                    'quote' => html_entity_decode($quote),
-                    'author' => $author,
-                    'category' => $category
-                );
-
-            // Push to Quotes Array
-            array_push($quotes_arr, $quotes_item);
-            // Turn to JSON & Output
-            echo json_encode($quotes_arr);
-        }
-        }
-    } else { 
-        // No Quotes
-        echo json_encode(
-            array('message' => 'No Quotes Found')
-        );
-    } */
